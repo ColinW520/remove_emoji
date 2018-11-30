2776d288c390f02de50069ab81f3f148a60449dc
Colin Hagan <colinhagan@gmail.com>
2018-11-30 15:43:38 -0600

    adds support for designating a replacement value

--- lib/remove_emoji.rb
+++ lib/remove_emoji.rb
@@ -4,8 +4,9 @@ require "remove_emoji/rules"
 module RemoveEmoji
   class Sanitize
 
-    def initialize(original_string)
+    def initialize(original_string, with: "")
       @original_string = original_string
+      @with = with
     end
 
     def self.call(*args)
@@ -13,7 +14,7 @@ module RemoveEmoji
     end
 
     def sanitize
-      @original_string.gsub(RemoveEmoji::MATCH_EMOJI_CODEPOINTS_RULE, '')
+      @original_string.gsub(RemoveEmoji::MATCH_EMOJI_CODEPOINTS_RULE, @with)
     end
 
   end
